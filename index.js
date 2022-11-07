fe2b6a2414873251f3bd4235350b48f776fb1ca1
Saqib Ahmad <you@example.com>
2022-11-07 12:31:30 +0600

    read method implemented

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.csyc5ob.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(uri);
+
+async function run() {
+    try {
+        const productCollection = client.db('ema-john').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally { }
+}
+run().catch(e => console.log(e))
 
 app.get('/', (req, res) => {
     res.send("ema john server running");
